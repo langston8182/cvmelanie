4d0b0930ca79d79b1ff143b9b15698e7395eca9e
Cyril Marchive <cyril.marchive@gmail.com>
2017-10-17 22:05:55 +0200

    Initialisation du projet

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name melanieCvApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the melanieCvApp
+ */
+angular.module('melanieCvApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name melanieCvApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the melanieCvApp
+ */
+angular.module('melanieCvApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
